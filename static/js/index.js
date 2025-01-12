fd7cbc8f4baf598f716cca7dd76270eeb7001ed0
DK <123861865+Kileorguy@users.noreply.github.com>
2025-01-13 03:45:27 +0700

    Feat: RAG and Flask Application

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log('Form submission prevented!');
+    });
+});
